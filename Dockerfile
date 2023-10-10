f76465d24ce1f3dbc7db8ecebc33f1066b1df45c
Connor Moore <cmoore@aurorasolar.com>
2023-10-09 18:19:20 -0700

    shifted to a docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
 FROM ubuntu
-WORKDIR /app
+# set work directory
+WORKDIR /usr/src/app
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install requirements
 RUN apt-get update
 RUN apt-get install -y python3 python3-pip
 RUN pip install uvicorn
 COPY . .
 RUN pip install -r requirements.txt
-EXPOSE 8000
-CMD ["uvicorn", "server:app"]
--- server.py
+++ server.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from manager import *
 app = FastAPI()
 
-@app.get("/users/{user}")
-def users(user: str):
-    output_user_info_file(user)
+@app.get("/")
+def ping():
+    return {"msg": "Hello World"}
 
-@app.get("/friends/{user}")
-def friends(user: str):
-    output_friends_file(user)
-
-@app.get("/transactions/{user}")
-def transactions(user: str):
-    output_transactions_file(user)
+# @app.get("/users/{user}")
+# def users(user: str):
+#     output_user_info_file(user)
+# 
+# @app.get("/friends/{user}")
+# def friends(user: str):
+#     output_friends_file(user)
+# 
+# @app.get("/transactions/{user}")
+# def transactions(user: str):
+#     output_transactions_file(user)
